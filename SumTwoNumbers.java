5b6dee6cb925feb269ad00254efae2b2b2c47b9f
vikash987 <40848251+vikash987@users.noreply.github.com>
2026-02-08 11:51:18 +0530

    Create SumTwoNumbers.java

---
+++ SumTwoNumbers.java
@@ -0,0 +1,12 @@
+public class SumTwoNumbers {
+
+    public static void main(String[] args) {
+
+        int first = 1;
+        int second = 2;
+
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
